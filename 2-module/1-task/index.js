0496dd5e7802443758c7d2baa770b420c475c5f0
kirill Provalov <doedro85@gmail.com>
2022-06-05 01:23:41 +0600

    решил задачи

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,15 @@
 function sumSalary(salaries) {
   // ваш код...
+  let sum = 0;
+  for (const key in salaries) {
+    const num = salaries[key];
+    
+    if (Number.isFinite(num)) {
+      sum += num;
+    }
+
+
+  }
+  return sum;
+  
 }
